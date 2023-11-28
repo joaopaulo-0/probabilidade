190ed56c76f03ac765d259f77e73bdc385e0fe21
joaopaulo-0 <145378314+joaopaulo-0@users.noreply.github.com>
2023-11-28 20:07:03 -0300

    Modificações

---
+++ scripts.js
@@ -0,0 +1,28 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f4f4f4;
+  }
+  
+  .container {
+    text-align: center;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  label, input, button {
+    margin: 10px 0;
+  }
+  
+  .charts {
+    display: flex;
+    justify-content: space-around;
+  }
+  
