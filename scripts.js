e265cc080816cdac0541246333d937c7359c877f
joaopaulo-0 <145378314+joaopaulo-0@users.noreply.github.com>
2023-11-28 20:15:44 -0300

    Modificações 1.0

--- scripts.js
+++ scripts.js
@@ -1,28 +1,38 @@
-body {
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: #f4f4f4;
-  }
-  
-  .container {
-    text-align: center;
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
-  
-  label, input, button {
-    margin: 10px 0;
-  }
-  
-  .charts {
-    display: flex;
-    justify-content: space-around;
-  }
-  
+function generateBernoulli() {
+  const p = parseFloat(document.getElementById('probability').value);
+
+  const nodes = new vis.DataSet([
+    { id: 0, label: '0', x: 0, y: 1 - p },
+    { id: 1, label: '1', x: 1, y: p }
+  ]);
+
+  const edges = new vis.DataSet([
+    { from: 0, to: 1 }
+  ]);
+
+  const container = document.getElementById('visualization');
+  const data = {
+    nodes: nodes,
+    edges: edges
+  };
+
+  const options = {
+    width: '600px',
+    height: '400px',
+    nodes: {
+      shape: 'dot',
+      scaling: {
+        min: 10,
+        max: 30,
+        label: {
+          min: 8,
+          max: 30,
+          drawThreshold: 12,
+          maxVisible: 20
+        }
+      }
+    }
+  };
+
+  const network = new vis.Network(container, data, options);
+}
